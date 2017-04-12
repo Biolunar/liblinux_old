7ba3708d663652c717d351053158ec5c5facc8c8
Mahdi Khanalizadeh <mahdi@khanalizadeh.com>
2017-04-12 14:42:35 +0200

    Added tests for mmap syscall.

--- include/liblinux/linux.h
+++ include/liblinux/linux.h
@@ -157,6 +157,47 @@ enum
 	linux_SEEK_MAX  = linux_SEEK_HOLE,
 };
 
+enum
+{
+	linux_PROT_READ      = 0x1,        // page can be read
+	linux_PROT_WRITE     = 0x2,        // page can be written
+	linux_PROT_EXEC      = 0x4,        // page can be executed
+	linux_PROT_SEM       = 0x8,        // page may be used for atomic ops
+	linux_PROT_NONE      = 0x0,        // page can not be accessed
+	linux_PROT_GROWSDOWN = 0x01000000, // mprotect flag: extend change to start of growsdown vma
+	linux_PROT_GROWSUP   = 0x02000000, // mprotect flag: extend change to end of growsup vma
+};
+
+enum
+{
+	linux_MAP_SHARED        = 0x01,      // Share changes
+	linux_MAP_PRIVATE       = 0x02,      // Changes are private
+	linux_MAP_TYPE          = 0x0f,      // Mask for type of mapping
+	linux_MAP_FIXED         = 0x10,      // Interpret addr exactly
+	linux_MAP_ANONYMOUS     = 0x20,      // don't use a file
+	linux_MAP_UNINITIALIZED = 0x4000000, // For anonymous mmap, memory could be uninitialized
+
+	linux_MAP_FILE          = 0,         // compatibility flag
+
+	linux_MAP_HUGE_SHIFT    = 26,
+	linux_MAP_HUGE_MASK     = 0x3f,
+
+	linux_MAP_GROWSDOWN     = 0x0100,    // stack-like segment
+	linux_MAP_DENYWRITE     = 0x0800,    // ETXTBSY
+	linux_MAP_EXECUTABLE    = 0x1000,    // mark it as an executable
+	linux_MAP_LOCKED        = 0x2000,    // pages are locked
+	linux_MAP_NORESERVE     = 0x4000,    // don't check for reservations
+	linux_MAP_POPULATE      = 0x8000,    // populate (prefault) pagetables
+	linux_MAP_NONBLOCK      = 0x10000,   // do not block on IO
+	linux_MAP_STACK         = 0x20000,   // give out an address that is best suited for process/thread stacks
+	linux_MAP_HUGETLB       = 0x40000,   // create a huge page mapping
+
+	linux_MAP_32BIT         = 0x40,      // only give out 32bit addresses
+
+	linux_MAP_HUGE_2MB      = (21 << linux_MAP_HUGE_SHIFT),
+	linux_MAP_HUGE_1GB      = (30 << linux_MAP_HUGE_SHIFT),
+};
+
 // All arguments have the same size as in the kernel sources.
 static inline LINUX_DEFINE_SYSCALL3_RET(read, linux_fd_t, fd, char*, buf, size_t, count, size_t)
 static inline LINUX_DEFINE_SYSCALL3_RET(write, linux_fd_t, fd, char const*, buf, size_t, count, size_t)
---
+++ tests/mmap.c
@@ -0,0 +1,65 @@
+#include "test.h"
+
+#include <limits.h>
+
+#include <liblinux/linux.h>
+
+#include <sys/mman.h>
+
+static enum TestResult test_invalid_file(void)
+{
+	int const fd = -1;
+	if (linux_mmap(0, sizeof(int), linux_PROT_READ, linux_MAP_PRIVATE, (linux_fd_t)fd, 0, 0) != linux_EBADF)
+		return TEST_RESULT_FAILURE;
+
+	return TEST_RESULT_SUCCESS;
+}
+
+static enum TestResult test_invalid_length(void)
+{
+	if (linux_mmap(0, 0, linux_PROT_READ, linux_MAP_PRIVATE | linux_MAP_ANONYMOUS, 0, 0, 0) != linux_EINVAL)
+		return TEST_RESULT_FAILURE;
+
+	return TEST_RESULT_SUCCESS;
+}
+
+static enum TestResult test_invalid_visibility(void)
+{
+	if (linux_mmap(0, sizeof(int), linux_PROT_READ, linux_MAP_ANONYMOUS, 0, 0, 0) != linux_EINVAL)
+		return TEST_RESULT_FAILURE;
+
+	if (linux_mmap(0, sizeof(int), linux_PROT_READ, linux_MAP_PRIVATE | linux_MAP_SHARED | linux_MAP_ANONYMOUS, 0, 0, 0) != linux_EINVAL)
+		return TEST_RESULT_FAILURE;
+
+	return TEST_RESULT_SUCCESS;
+}
+
+static enum TestResult test_alloc(void)
+{
+	void* ret = 0;
+	size_t const length = sizeof(int);
+	if (linux_mmap(0, length, linux_PROT_WRITE, linux_MAP_PRIVATE | linux_MAP_ANONYMOUS, 0, 0, &ret))
+		return TEST_RESULT_FAILURE;
+
+	int* const p = ret;
+	*p = INT_MAX;
+
+	if (munmap(ret, length) == -1)
+		return TEST_RESULT_OTHER_FAILURE;
+
+	return TEST_RESULT_SUCCESS;
+}
+
+int main(void)
+{
+	int ret = EXIT_SUCCESS;
+
+	printf("Start testing mmap.\n");
+	DO_TEST(invalid_file, &ret);
+	DO_TEST(invalid_length, &ret);
+	DO_TEST(invalid_visibility, &ret);
+	DO_TEST(alloc, &ret);
+	printf("Finished testing mmap.\n");
+
+	return ret;
+}
